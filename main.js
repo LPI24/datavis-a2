3856b7287e49965ec3b1e3077906fc6257c55bc8
LP24 <lukas.pilz1@mailbox.tu-dresden.de>
2025-11-10 22:03:39 +0100

    initial commit LP

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 // Waiting until document has loaded
 window.onload = () => {
 
-  // YOUR CODE GOES HERE
-  console.log("YOUR CODE GOES HERE");
+  // specifing charts dimension
+
+  const width = 928;
+  const height = 600;
+  const marginTop = 25;
+  const marginRight = 20;
+  const marginBottom = 35;
+  const marginLeft = 40;
+
+  //Creating the SVG Container
+  const svg = d3.select("#scatter_plot")
+    .append("svg")
+      .attr("width", width + marginLeft + marginRight)
+      .attr("heigth", height + marginBottom + marginTop)
+    .append("g")
+
 
   // Load the data set from the assets folder:
 
